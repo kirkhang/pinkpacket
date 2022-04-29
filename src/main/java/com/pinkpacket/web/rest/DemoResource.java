d9468bb4294b46813756212e43e67793703a892f
kirk <kirkzhang@youtap.com>
2022-04-30 02:07:35 +1200

    add redis

--- src/main/java/com/pinkpacket/web/rest/DemoResource.java
+++ src/main/java/com/pinkpacket/web/rest/DemoResource.java
@@ -1,10 +1,8 @@
-package com.pinkpacket.resource;
+package com.pinkpacket.web.rest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/api")
